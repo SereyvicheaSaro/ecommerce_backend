aae4d39c17a1884eb8308e004d7104fdea43215c
Vichea <serey.vichea9999@gmail.com>
2023-12-20 15:03:06 +0700

    update: migraion and the fucking seed

--- database/migrations/2023_12_08_020436_create_products.php
+++ database/migrations/2023_12_08_020436_create_products.php
@@ -6,24 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->bigInteger('categories_id')->unsigned();
-            $table->text('description')->nullable();
+            $table->bigInteger('category_id')->unsigned();
             $table->double('pricing');
+            $table->text('description')->nullable();
+            $table->jsonb('images')->nullable();
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
